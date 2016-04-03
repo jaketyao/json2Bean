0ede64a67740662049e6ad1176d16a875477e057
Young_mac <itboye004@163.com>
2016-04-03 15:07:57 +0800

    bean中字段 按字母顺序排序

--- json2bean/src/com/json2bean/CamelCaseBeanGenerator.java
+++ json2bean/src/com/json2bean/CamelCaseBeanGenerator.java
@@ -4,11 +4,16 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.Map.Entry;
+
 /**
  * 驼峰式写法
+ * 
  * @author Young
  *
  */
@@ -20,104 +25,126 @@ public class CamelCaseBeanGenerator extends MyBeanGenerator {
 
 	@Override
 	public void writeBean(String className, Map<String, Object> map) throws IOException {
-		File file=new File("src/"+packName.replace(".", "/"));
-		if (!file.exists()||file.exists()&&file.isFile()) {
+		File file = new File("src/" + packName.replace(".", "/"));
+		if (!file.exists() || file.exists() && file.isFile()) {
 			file.mkdirs();
 		}
-		BufferedWriter bw=new BufferedWriter(new FileWriter(new File(file,  className+".java")));
+
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(file, className + ".java")));
 		bw.write("package ");
 		bw.write(packName);
 		bw.write(";\n");
 		bw.write("import java.util.List;\n\n");
-		
+
 		bw.write("/**\n");
 		bw.write(" *auto generate\n");
 		bw.write(" *\n");
 		bw.write(" *@author Young\n");
 		bw.write(" *\n");
 		bw.write(" */\n");
-		
-		bw.write("public class ");bw.write(className); bw.write("{\n");
-		
-		Set<Entry<String, Object>> set=map.entrySet();
-		
+
+		bw.write("public class ");
+		bw.write(className);
+		bw.write("{\n");
+
+		map=sortMapByKey(map);
+		Set<Entry<String, Object>> set = map.entrySet();
+
 		for (Entry<String, Object> entry : set) {
 			bw.write("    ");
-			bw.write(entry.getValue().toString()); bw.write(" "); bw.write(formatReference(entry.getKey()));bw.write(";\n");
+			bw.write(entry.getValue().toString());
+			bw.write(" ");
+			bw.write(formatReference(entry.getKey()));
+			bw.write(";\n");
 		}
 		bw.write("\n");
-		set=map.entrySet();
-		
+		set = map.entrySet();
+
 		for (Entry<String, Object> entry : set) {
-		
-			bw.write("    public "); bw.write(entry.getValue().toString());
-			bw.write(" get"); bw.write(capitalUpper(entry.getKey()));
+
+			bw.write("    public ");
+			bw.write(entry.getValue().toString());
+			bw.write(" get");
+			bw.write(capitalUpper(entry.getKey()));
 			bw.write("(){\n");
 			bw.write("        ");
 			bw.write("return ");
-			bw.write( formatReference(entry.getKey()));
-			
+			bw.write(formatReference(entry.getKey()));
+
 			bw.write(";\n    }\n\n");
-			
+
 			//////////////////////////
-			
-			bw.write("    public void ");  
-			bw.write("set"); bw.write(capitalUpper(entry.getKey()));
-			bw.write("("); bw.write(entry.getValue().toString()); bw.write(" ");
+
+			bw.write("    public void ");
+			bw.write("set");
+			bw.write(capitalUpper(entry.getKey()));
+			bw.write("(");
+			bw.write(entry.getValue().toString());
+			bw.write(" ");
 			bw.write(formatReference(entry.getKey()));
 			bw.write("){\n");
 			bw.write("        ");
 			bw.write("this.");
-			bw.write( formatReference(entry.getKey()));
+			bw.write(formatReference(entry.getKey()));
 			bw.write("=");
 			bw.write(formatReference(entry.getKey()));
 			bw.write(";\n    }\n");
-			
-			
+
 			bw.write("\n");
-			
-			
+
 		}
 		bw.write("}");
-		
-		
+
 		bw.close();
 	}
-	
-	private String capitalUpper(String getset){
-		String s=formatReference(getset);
-		char[]chs=s.toCharArray();
-		if (chs[0]>'a'&&chs[0]<'z') {
-			chs[0]=(char) (chs[0]-32);
-		}
+
+	public Map<String, Object> sortMapByKey(Map<String, Object> oriMap) {
 		
-		return new String(chs);
-		
+		Map<String, Object> sortedMap = new TreeMap<String, Object>(new Comparator<String>() {
+			public int compare(String key1, String key2) {
+
+				return  key1.compareTo(key2);
+			}
+		});
+		sortedMap.putAll(oriMap);
+		return sortedMap;
 	}
-	private String formatReference(String ref){
-		
-		char[]chs=ref.toCharArray();
-		
-		if (chs[0]>='A'&&chs[0]<='Z') {
-			chs[0]=(char) (chs[0]+32);
+
+	private String capitalUpper(String getset) {
+		String s = formatReference(getset);
+		char[] chs = s.toCharArray();
+		if (chs[0] > 'a' && chs[0] < 'z') {
+			chs[0] = (char) (chs[0] - 32);
 		}
-		StringBuilder stringBuilder=new StringBuilder();
+
+		return new String(chs);
+
+	}
+
+	private String formatReference(String ref) {
+
+		char[] chs = ref.toCharArray();
+
+		if (chs[0] >= 'A' && chs[0] <= 'Z') {
+			chs[0] = (char) (chs[0] + 32);
+		}
+		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append(chs[0]);
 		for (int i = 1; i < chs.length; i++) {
-			if (chs[i]=='_') {
+			if (chs[i] == '_') {
 				continue;
 			}
-			if (chs[i-1]=='_') {
-				if (chs[i]>='a'&&chs[i]<='z') {
-					stringBuilder.append((char)(chs[i]-32));
-				}else{
+			if (chs[i - 1] == '_') {
+				if (chs[i] >= 'a' && chs[i] <= 'z') {
+					stringBuilder.append((char) (chs[i] - 32));
+				} else {
 					stringBuilder.append(chs[i]);
 				}
-			}else{
+			} else {
 				stringBuilder.append(chs[i]);
 			}
 		}
-		
+
 		return stringBuilder.toString();
 	}
 }
--- json2bean/src/com/json2bean/MyBeanGenerator.java
+++ json2bean/src/com/json2bean/MyBeanGenerator.java
@@ -4,112 +4,131 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class MyBeanGenerator implements BeanGenerator {
 
 	String packName;
+
 	public MyBeanGenerator(String packName) {
 		// TODO Auto-generated constructor stub
-		this.packName=packName;
+		this.packName = packName;
 	}
 
 	@Override
 	public void writeBean(String className, Map<String, Object> map) throws IOException {
-		File file=new File("src/"+packName.replace(".", "/"));
-		if (!file.exists()||file.exists()&&file.isFile()) {
+		File file = new File("src/" + packName.replace(".", "/"));
+		if (!file.exists() || file.exists() && file.isFile()) {
 			file.mkdirs();
 		}
-		BufferedWriter bw=new BufferedWriter(new FileWriter(new File(file,  className+".java")));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(file, className + ".java")));
 		bw.write("package ");
 		bw.write(packName);
 		bw.write(";\n");
 		bw.write("import java.util.List;\n\n");
-		
+
 		bw.write("/**\n");
 		bw.write(" *auto generate\n");
 		bw.write(" *\n");
 		bw.write(" *@author Young\n");
 		bw.write(" *\n");
 		bw.write(" */\n");
-		
-		bw.write("public class ");bw.write(className); bw.write("{\n");
-		
-		Set<Entry<String, Object>> set=map.entrySet();
-		
+
+		bw.write("public class ");
+		bw.write(className);
+		bw.write("{\n");
+		map=sortMapByKey(map);
+		Set<Entry<String, Object>> set = map.entrySet();
+
 		for (Entry<String, Object> entry : set) {
 			bw.write("    ");
-			bw.write(entry.getValue().toString()); bw.write(" "); bw.write(entry.getKey());bw.write(";\n");
+			bw.write(entry.getValue().toString());
+			bw.write(" ");
+			bw.write(entry.getKey());
+			bw.write(";\n");
 		}
 		bw.write("\n");
-		set=map.entrySet();
 		
+		set = map.entrySet();
+
 		for (Entry<String, Object> entry : set) {
-		
-			bw.write("    public "); bw.write(entry.getValue().toString());
-			bw.write(" get"); bw.write(capitalUpperCase(entry.getKey()));
+
+			bw.write("    public ");
+			bw.write(entry.getValue().toString());
+			bw.write(" get");
+			bw.write(capitalUpperCase(entry.getKey()));
 			bw.write("(){\n");
 			bw.write("        ");
 			bw.write("return ");
-			bw.write( entry.getKey());
-			
+			bw.write(entry.getKey());
+
 			bw.write(";\n    }\n\n");
-			
+
 			//////////////////////////
-			
-			bw.write("    public void ");  
-			bw.write("set"); bw.write(capitalUpperCase(entry.getKey()));
-			bw.write("("); bw.write(entry.getValue().toString()); bw.write(" ");
+
+			bw.write("    public void ");
+			bw.write("set");
+			bw.write(capitalUpperCase(entry.getKey()));
+			bw.write("(");
+			bw.write(entry.getValue().toString());
+			bw.write(" ");
 			bw.write(entry.getKey());
 			bw.write("){\n");
 			bw.write("        ");
 			bw.write("this.");
-			bw.write( entry.getKey());
+			bw.write(entry.getKey());
 			bw.write("=");
 			bw.write(entry.getKey());
 			bw.write(";\n    }\n");
-			
-			
+
 			bw.write("\n");
-			
-			
+
 		}
 		bw.write("}");
-		
-		
+
 		bw.close();
 	}
 
-	private String capitalUpperCase(String s){
-		char[]chs=s.toCharArray();
-		if (chs[0]>='a'&&chs[0]<='z') {
-			chs[0]=(char) (chs[0]-32);
+	private String capitalUpperCase(String s) {
+		char[] chs = s.toCharArray();
+		if (chs[0] >= 'a' && chs[0] <= 'z') {
+			chs[0] = (char) (chs[0] - 32);
 		}
 		return new String(chs);
-		
+
 	}
+
 	@Override
 	public void writeList(String list) throws IOException {
-		
-		File file=new File("src/"+packName.replace(".", "/"));
-		if (!file.exists()||file.exists()&&file.isFile()) {
+
+		File file = new File("src/" + packName.replace(".", "/"));
+		if (!file.exists() || file.exists() && file.isFile()) {
 			file.mkdirs();
 		}
-		
-		BufferedWriter bw=new BufferedWriter(new FileWriter( new File(file,list.replaceAll("<|>", "_")+".txt")));
-		
-		 bw.write(list);
-		 bw.write(";");
-		
+
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(file, list.replaceAll("<|>", "_") + ".txt")));
+
+		bw.write(list);
+		bw.write(";");
+
 		bw.close();
-		
+
 	}
 
-	 
+	public Map<String, Object> sortMapByKey(Map<String, Object> oriMap) {
 
+		Map<String, Object> sortedMap = new TreeMap<String, Object>(new Comparator<String>() {
+			public int compare(String key1, String key2) {
 
-	 
+				return key1.compareTo(key2);
+			}
+		});
+		sortedMap.putAll(oriMap);
+		return sortedMap;
+	}
 
 }
